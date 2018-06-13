5de3150a3b45af5739bc52c81ab4449398b85fd9
l0971 <liy0971@dtdream.com>
2018-06-13 14:19:06 +0800

    config 客户端

---
+++ config-client/src/main/java/com/dtdream/configclient/web/HelloController.java
@@ -0,0 +1,19 @@
+package com.dtdream.configclient.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class HelloController {
+
+    @Value("${demo.title}")
+    String param;
+
+    @RequestMapping(value = "/hi")
+    public String hi() {
+        return param;
+    }
+}
